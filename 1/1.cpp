7b7b1c2cafd4252d850d63df07f6704781a6e3b9
Maxim <Maxim@MAXIMPC>
2016-12-29 19:49:42 +0200

    Добавьте файлы проекта.

---
+++ 1/1.cpp
@@ -0,0 +1,25 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+/*
+int hui()
+{
+const int x = 4;
+int i = 0, z = 0;
+for (i; i < x; i++)
+{
+z++;
+cout << z<<"\n";
+}
+return 0;
+}
+*/
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	//hui();
+	return 0;
+}
