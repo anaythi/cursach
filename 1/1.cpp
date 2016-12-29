b9c5ae780b25a03140baf625c202d8b64245aecd
Maxim <Maxim@MAXIMPC>
2016-12-29 21:25:35 +0200

    Тест

--- 1/1.cpp
+++ 1/1.cpp
@@ -6,13 +6,10 @@
 using namespace std;//команда для работы операторов cin и cout
 int menu() //функция меню
 {
-/*const int x = 4;
-int i;
-*/
-cout << "1: \t 1 \n" << "2: \t 2 \n" << "3: \t 3 \n" << "4: \t 4 \n"; //тестовый вывод
+//const int x = 4;
+cout << "1: \t 1 \n" << "2: \t 2 \n" << "3: \t 3 \n" << "4: \t 4 \n"<<"Выберете элемент меню(1-4): "; //тестовый вывод
 return 0;
 }
-/*
 int dmassiv()
 {
 int *t = new int[10];
@@ -24,10 +21,20 @@ cout << t[i] << "\n";
 }
 return 0;
 }
-*/
+
 int main() //главная ф-ция
 {
+	int i;
 	menu(); //вызов функции меню
+	cin >> i;
+	switch (i)
+	{
+	case 1: cout << "\n ty \n"; break;
+	case 2: cout << "\n tochno \n"; break;
+	case 3: cout << "\n ne \n"; break;
+	case 4: cout << "\n pidor \n"; break;
+	default: cout << "Pidor \n"; break;
+	}
 	//dmassiv();
 	return 0;
 }
